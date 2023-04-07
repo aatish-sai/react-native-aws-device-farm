d7699e96449db24ee08e5cfda231ff00dd1b82b0
Aatish Sai <meatish.sai@gmail.com>
2023-04-08 02:20:01 +0545

    ci: debug

---
+++ wait-script.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+stauts=0
+while [ $status -eq 0]
+do
+    state=$((aws devicefarm get-upload --arn $1 | jq '.upload.status'))
+    if [$state == "FAILED"]
+    then
+        $status = 1
+    fi
+    if [$state = "SUCCEEDED"]
+    then
+        $status = 1
+    fi
+    sleep 5
+done
