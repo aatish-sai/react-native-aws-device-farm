4a1db1614095208716b2256761eb6b71cfb8c171
Aatish Sai <meatish.sai@gmail.com>
2023-04-08 02:45:01 +0545

    ci: debug

--- wait-script.sh
+++ wait-script.sh
@@ -3,11 +3,11 @@ status=0
 while [[ $status -eq 0 ]]; do
     state=$(aws devicefarm get-upload --arn $1 | jq '.upload.status')
     echo "State is $state"
-    if [[ $state == "FAILED" ]]
+    if [[ "$state" == "FAILED" ]]
     then
         status=1
     fi
-    if [[ $state == "SUCCEEDED" ]]
+    if [[ "$state" == "SUCCEEDED" ]]
     then
         status=1
     fi
