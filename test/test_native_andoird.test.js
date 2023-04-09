2c313f8a399b4549824a0d61650a631b53212bda
Aatish Sai <meatish.sai@gmail.com>
2023-04-09 16:10:01 +0545

    chore: added appium test

--- test/test_native_andoird.test.js
+++ test/test_native_andoird.test.js
@@ -2,19 +2,24 @@ var expect = require('chai').expect;
 
 var wd = require('wd'),
     driver = wd.promiseChainRemote({
-        host: '127.0.0.1',
+        host: 'localhost',
         port: 4723
     });
 
 var assert = require('assert');
-describe('AWSDeviceFarmReferenceAppTest', function () {
-    
+describe('AWSDeviceFarmReferenceAppTest', function() {
+
     before(function() {
         this.timeout(300 * 1000);
         return driver.init();
-        });
+    });
 
-        after(function() {
-            console.log("Quitting");
-        });
+    after(function() {
+        console.log("Quitting");
+    });
+
+    it('test_app_is_loaded', async function() {
+        const element = await driver.elementById('me.aatish.reactnativeawsdevicefarm:id/action_bar_root')
+        expect(element).to.exist;
+    });
 })
